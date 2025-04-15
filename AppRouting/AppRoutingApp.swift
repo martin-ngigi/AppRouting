810419dd000fa32cfaaf270e9bd548980374731d
Martin Wainaina <martinwainaina001@gmail.com>
2025-04-15 15:36:34 +0300

    Initial Commit

---
+++ AppRouting/AppRoutingApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppRoutingApp.swift
+//  AppRouting
+//
+//  Created by Martin on 15/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct AppRoutingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
